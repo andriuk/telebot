36c5becfaf75f3a6282f42ff5c0e43dfa821045c
yuandrk <62573046+andriuk@users.noreply.github.com>
2023-11-09 23:02:25 +0000

    rebuild project

--- main.go
+++ main.go
@@ -1,48 +1,11 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-	"github.com/spf13/cobra"
-	"gopkg.in/telebot.v3"
-	"log"
-	"os"
-	"time"
-)
+import "github.com/andriuk/telebot/cmd"
 
-// Telegram bot
-var (
-	TeleToken = os.Getenv("TELEGRAM_TOKEN")
-)
-
-// kbotCmd represents the kbot command
-
-var kbotCmd = &Command{
-	Use:   "kbot",
-	Short: "kbot is a telegram bot",
-	Long:  `kbot is a telegram bot`,
-
-	Run: func(cmd *cobra.command, args []string) {
-
-		fmt.Printf("kbot %s started", appVersion)
-		log.Println("kbot is running")
-
-		kbot, err := telebot.NewBot(telebot.Settings{
-			URL:    "",
-			Token:  TeleToken,
-			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
-		})
-		if err != nil {
-			log.Fatal(err)
-			log.Fatalf("Please check TELEGRAM_TOKEN env variable %s", err)
-			return
-		}
-
-		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
-			return err
-		})
-
-		kbot.Start()
-
-	},
+func main() {
+	cmd.Execute()
 }
